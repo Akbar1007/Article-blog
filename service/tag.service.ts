7d83be810ff20cc62e92fa09f4ae4a462b248c68
Akbar1007 <a.akbarjon007@gmail.com>
2025-04-19 09:45:32 +0500

    tags

---
+++ service/tag.service.ts
@@ -0,0 +1,44 @@
+import { IBlog } from '@/types'
+import request, { gql } from 'graphql-request'
+
+const graphQLAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT!
+
+export const getBlogsByTag = async (slug: string) => {
+	const query = gql`
+		query MyQuery($slug: String!) {
+			tag(where: { slug: $slug }) {
+				blog {
+					description
+					author {
+						name
+						image {
+							url
+						}
+						bio
+					}
+					content {
+						html
+					}
+					createdAt
+					image {
+						url
+					}
+					slug
+					tag {
+						name
+						slug
+					}
+					title
+				}
+				name
+			}
+		}
+	`
+
+	const { tag } = await request<{ tag: { blog: IBlog[]; name: string } }>(
+		graphQLAPI,
+		query,
+		{ slug }
+	)
+	return tag
+}
